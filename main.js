c50193dfd59b82578016a9a89f3a0de534aa9017
Alexander Yu <yua@berkeley.edu>
2020-12-28 19:52:01 -0800

    about page

--- main.js
+++ main.js
@@ -90,6 +90,24 @@ const adjustIcons = function(event) {
     }
 }
 
+const aboutGeneral = function(event) {
+    document.getElementById("profile-photo").src = "media/profile.jpeg";
+    document.getElementById("about-text").innerHTML = "Hi! I'm Alex, an engineering student at UC Berkeley who loves building things.<br/><br/>I'm a super hands-on type of learner who's constantly looking for opportunities to tackle new challenges and solve problems.<br/><br/>In my free time I love rock climbing, skiing, playing poker, and hanging out with friends. I've also started dabbling in photography and video editing.";
+}
+
+const aboutWushu = function(event) {
+    document.getElementById("profile-photo").src = "media/wushu.JPG";
+    document.getElementById("about-text").innerHTML = "I was a two-time world champion, two-time pan-american champion, and multi-time national champion in Wushu (Chinese martial arts). <br/><br/>My parents were both professional Wushu athletes from China. My father grew up training on the Beijing Wushu Team with actor Jet-Li. My mother won 32 gold medals in her professional career.<br/><br/>I basically grew up at my family's Wushu school and started training at the age of four. By high school, I was regularly competing for Team USA in international Wushu and Taijiquan competitions. Most recently, I competed for Team USA in Bulgaria in 2018 for the 3rd World Taijiquan Championships. Click <a href='https://youtu.be/EgfUgZUINJE?t=7317' target='_blank'>here</a> for a video."
+}
+
+const aboutEngineering = function(event) {
+    document.getElementById("profile-photo").src = "media/chassis.png";
+    document.getElementById("about-text").innerHTML = "My passion for engineering started when I was very young. I've loved building things for as long as I can remember. I grew up doing things like Science Olympiad and robotics.<br/><br/>In my freshman year of college, I joined Berkeley Formula Racing where I was a member of the electrical department. <br/><br>Currently, I'm pursuing my passion for engineering through personal projects and I'm an incoming Engineering Intern at <a href='https://www.sofi.com/' target='_blank'>SoFi</a>.";
+}
+
 window.addEventListener("scroll", navAdjust);
 window.addEventListener("scroll", navPointer);
 window.addEventListener("scroll", adjustIcons);
+document.getElementById("about-general").addEventListener("click", aboutGeneral);
+document.getElementById("about-wushu").addEventListener("click", aboutWushu);
+document.getElementById("about-engineering").addEventListener("click", aboutEngineering);
